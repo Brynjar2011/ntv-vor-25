27a9aa7a56adde21a65577bcae58373a5f9c9148
Haukur81 <haukurg81@gmail.com>
2025-02-17 20:21:54 +0000

    Tími 1

---
+++ cheatSheet/cheatshee01_numbers.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+import 'dart:convert';
+
+void main (List<String> arguments) {
+
+  /*print("Hvað er uppáhaldstalan þín?");
+  String inputNumber = stdin.readLineSync().toString();
+  print("Hún er $inputNumber");
+
+  int result = 5 * int.parse(inputNumber);
+  print("5 sinnum $inputNumber er $result");
+
+
+  int number1 = 8;
+  int number2 = 3;
+  double result2 = number1 / number2;
+  print (result2.toStringAsFixed(3));
+
+  print("Hæ, hvað heitir þú?");
+  String firstName = stdin.readLineSync(encoding: utf8).toString();
+  print("Hello $firstName");
+
+  print("Hvað er eftirnafn þitt?");
+  String lastName = stdin.readLineSync(encoding: utf8).toString();
+  firstName = firstName.replaceAll(firstName, "Jón");
+  lastName = lastName.replaceAll(lastName, "Jónsson");
+  print("Hello $firstName $lastName");
+  */
+
+  String firstName = "Haukur Doddi";
+  String lastName = "Gunnarsson";
+  String fullName = firstName + " " + lastName;
+  String url = "ntv.is";
+
+  List<String> names = firstName.split(" ");
+  print(names);
+  print(names [0]);
+  print(names[1]);
+
+  String username = names[0] + names[1].substring(0,1) + lastName.substring(0,3);
+  username = username.toLowerCase();
+  print(username);
+
+  String emailAddress = username + "@" + url;
+  print(emailAddress);
+
+}
---
+++ cheatSheet/cheatsheet01.dart
@@ -0,0 +1,25 @@
+void main (List<String> arguments) {
+
+  String firstName = "Haukur";
+  String lastName = "Gunnarsson";
+  print("Hi $firstName $lastName");
+  //Hér prentar hún út Haukur Gunnarsson
+  String fullName = firstName + " " + lastName;
+  print("Hi $fullName");
+
+  String SSN = "031181-3569";
+  String fixSSN = SSN.replaceAll("-","");
+  //replaceAll er notað til þess að taka í burtu fyrri hlutann ss "-"
+  int SSNLength = fixSSN.length;
+  print(SSNLength);
+
+  String message = "Hello World";
+
+  String fixMessage = message.replaceAll ("World", "NTV");
+  print(fixMessage.toUpperCase());
+
+  String birthDate = SSN.substring(0,6);
+  print(birthDate);
+  String email = "HAUKURG81@gmail.com";
+  print(email.toLowerCase());
+}
