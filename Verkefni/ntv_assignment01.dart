026172eea5ee8bd568e9a0c4ab74c7833f057a76
Haukur81 <haukurg81@gmail.com>
2025-02-23 20:55:30 +0000

    Update ntv_assignment01.dart

--- Verkefni/ntv_assignment01.dart
+++ Verkefni/ntv_assignment01.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
-
 void main (List <String> arguments) {
 
   print("Hello \nHaukur");
+  print("Hæ");
+  print("Haukur");
 
   int h = 1;
   int i = 2;
@@ -15,9 +16,16 @@ void main (List <String> arguments) {
   print(no);
   print (n/o);
 
-  
+  int result1 = (-1 + 5 *5);
+  print (result1);
 
+  int result2 = (25+5) % 4 ;
+  print (result2);
 
+  double result3 = (8+6 * 4/2);
+  print (result3.toStringAsFixed(0));
 
+  double result4 = (2+ 15 / 6* 1 - 7 % 2);
+  print (result4.toStringAsFixed(1));
 
 }
