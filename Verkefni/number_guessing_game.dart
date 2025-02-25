79f69bc7f6967107ffed6e0410311e166b815d8c
Haukur81 <haukurg81@gmail.com>
2025-02-25 23:06:45 +0000

    Update number_guessing_game.dart

--- Verkefni/number_guessing_game.dart
+++ Verkefni/number_guessing_game.dart
@@ -1,52 +1,38 @@
 import 'dart:io';
 import 'dart:math';
-
-void main (){
-
-
-  int randomNumber = Random().nextInt(100)+1;
-  print ("randomNumber $randomNumber");   //Tek þetta út svo talan sjáist ekki//
-
-  int totaltries = 0;
+void main () {
+  int randomNumber = Random().nextInt(100) + 1;
+  print("randomNumber $randomNumber"); //Tek þetta út svo talan sjáist ekki//
+  int totaltries = 1;
 
   bool game = false;
-  while(game== false) {
-
+  while (game == false) {
     print("Giskaðu á töluna");
     String askNumber = stdin.readLineSync().toString();
     int askNumber2 = int.parse(askNumber);
 
-
     if (askNumber2 == randomNumber) {
       print("Þetta var rétt");
-      print ("Þú þurftir $totaltries tilraunir");
+      print("Þú þurftir $totaltries tilraunir");
       game = true;
-
-      print("Viltu prófa aftur? Skrifaðu Y fyrir já og N fyrir nei");
-      String askTryAgain = stdin.readLineSync().toString();
-
-        bool retry = false;
-        while(retry == false) {
-        if (askTryAgain == "Y");{
-          print("Giskaðu á töluna");
-          String askNumber = stdin.readLineSync().toString();
-          int askNumber2 = int.parse(askNumber);
-              }
-            }
-        }
-
-    else if (askNumber2 > randomNumber){
-      totaltries +=1;
-      print("Of hátt, rétt tala er lægri");
-  }
-    else if (askNumber2 < randomNumber){
-      totaltries +=1;
-      print ("Of lágt, rétt tala er hærri");
     }
-
-
+     /* print("Viltu prófa aftur? Skrifaðu Y fyrir já og N fyrir nei");
+      String askTryAgain = stdin.readLineSync().toString();
+      if (askTryAgain == "Y");
+      {
+        print("Giskaðu á töluna");
+        if (askTryAgain == "Y");
+        {
+          print("Giskaðu á töluna");
+        }
+      }  */
+    else if (askNumber2 > randomNumber) {
+      totaltries += 1;
+      print("Of hátt, rétt tala er lægri");
+    }
+    else if (askNumber2 < randomNumber) {
+      totaltries += 1;
+      print("Of lágt, rétt tala er hærri");
+    }
   }
-
-  
-  
 }
