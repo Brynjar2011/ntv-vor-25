8a3f1322f9d9b12990d6c739c09d22e12cad5e29
Haukur81 <haukurg81@gmail.com>
2025-02-25 21:30:06 +0000

    Update number_guessing_game.dart

--- Verkefni/number_guessing_game.dart
+++ Verkefni/number_guessing_game.dart
@@ -2,9 +2,12 @@ import 'dart:io';
 import 'dart:math';
 
 void main (){
-  
+
+
   int randomNumber = Random().nextInt(100)+1;
-  print ("randomNumber $randomNumber");
+  print ("randomNumber $randomNumber");   //Tek þetta út svo talan sjáist ekki//
+
+  int totaltries = 0;
 
   bool game = false;
   while(game== false) {
@@ -13,30 +16,37 @@ void main (){
     String askNumber = stdin.readLineSync().toString();
     int askNumber2 = int.parse(askNumber);
 
+
     if (askNumber2 == randomNumber) {
       print("Þetta var rétt");
+      print ("Þú þurftir $totaltries tilraunir");
       game = true;
-    }
+
+      print("Viltu prófa aftur? Skrifaðu Y fyrir já og N fyrir nei");
+      String askTryAgain = stdin.readLineSync().toString();
+
+        bool retry = false;
+        while(retry == false) {
+        if (askTryAgain == "Y");{
+          print("Giskaðu á töluna");
+          String askNumber = stdin.readLineSync().toString();
+          int askNumber2 = int.parse(askNumber);
+              }
+            }
+        }
 
     else if (askNumber2 > randomNumber){
+      totaltries +=1;
       print("Of hátt, rétt tala er lægri");
   }
     else if (askNumber2 < randomNumber){
+      totaltries +=1;
       print ("Of lágt, rétt tala er hærri");
     }
 
-    else {
-      print("Rangt, reyndu aftur");
-    }
 
   }
 
-
-
-
-
-  
-  
   
   
 }
