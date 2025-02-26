233927b5e01b4c4222da53e4766805583d1161a9
Haukur81 <haukurg81@gmail.com>
2025-02-26 19:01:42 +0000

    Create class-05.dart

---
+++ cheatSheet/class-05.dart
@@ -0,0 +1,34 @@
+void main() {
+
+  var teamsPL = ["Liverpool", "Everton", "West Ham"];
+  print (teamsPL);
+
+  teamsPL [1] = "Fulham";
+  print(teamsPL);
+
+  teamsPL.add("Aston Villa");
+  print (teamsPL);
+
+  teamsPL.addAll(["Everton", "Man Shitty", "Arsenal"]);
+  print(teamsPL);
+
+  var emptyList = [];
+  print(emptyList);
+
+  teamsPL.insert(1, "Nott For");
+  print(teamsPL);
+
+  teamsPL.insertAll(4, ["Bmoth", "ManUtd"]);
+  print(teamsPL);
+
+  teamsPL.remove("ManUtd");
+  print(teamsPL);
+
+  teamsPL.removeAt(1);
+  print(teamsPL);
+
+  print(teamsPL.indexOf("Arsenal"));
+  teamsPL.removeAt(7);
+  print(teamsPL);
+
+}
