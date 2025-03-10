d96478964007f2bff0a33a1ca0e3e16d2ef04f72
Haukur81 <haukurg81@gmail.com>
2025-03-10 21:06:24 +0000

    10.03.2025

--- .idea/footballgame/logic.dart
+++ .idea/footballgame/logic.dart
@@ -1,44 +1,54 @@
-class footballGame {
-  footballTeam homeTeam;
-  footballTeam awayTeam;
-  int homeTeamScore;
-  int awayTeamScore;
-  int time;
+import 'dart:io';
+import 'logic.dart';
 
-  footballGame({
-    required this.homeTeam,
-    required this.awayTeam,
-    this.homeTeamScore = 0,
-    this.awayTeamScore = 0,
-    this.time = 0
-  });
-}
+void main() {
+  // Creating players for Barcelona
+  List<footballPlayer> barcelonaPlayers = [
+    footballPlayer(squadNumber: 1, firstName: "Marc-André", lastName: "ter Stegen", position: "Goalkeeper"),
+    footballPlayer(squadNumber: 2, firstName: "João", lastName: "Cancelo", position: "Defender"),
+    footballPlayer(squadNumber: 3, firstName: "Ronald", lastName: "Araújo", position: "Defender"),
+    footballPlayer(squadNumber: 4, firstName: "Andreas", lastName: "Christensen", position: "Defender"),
+    footballPlayer(squadNumber: 5, firstName: "Alejandro", lastName: "Balde", position: "Defender"),
+    footballPlayer(squadNumber: 6, firstName: "Frenkie", lastName: "de Jong", position: "Midfielder"),
+    footballPlayer(squadNumber: 7, firstName: "Pedri", lastName: "González", position: "Midfielder"),
+    footballPlayer(squadNumber: 8, firstName: "Gavi", lastName: "Paez", position: "Midfielder"),
+    footballPlayer(squadNumber: 9, firstName: "Robert", lastName: "Lewandowski", position: "Forward"),
+    footballPlayer(squadNumber: 10, firstName: "João", lastName: "Félix", position: "Forward"),
+    footballPlayer(squadNumber: 11, firstName: "Raphinha", lastName: "Belloli", position: "Forward"),
+  ];
 
-class footballTeam {
-  String name;
-  String league;
-  String country;
-  List<footballPlayer> players;
-  footballTeam({required this.name, required this.league,
-    required this.country, required this.players});
+  // Creating players for Liverpool
+  List<footballPlayer> liverpoolPlayers = [
+    footballPlayer(squadNumber: 1, firstName: "Alisson", lastName: "Becker", position: "Goalkeeper"),
+    footballPlayer(squadNumber: 2, firstName: "Trent", lastName: "Alexander-Arnold", position: "Defender"),
+    footballPlayer(squadNumber: 3, firstName: "Virgil", lastName: "van Dijk", position: "Defender"),
+    footballPlayer(squadNumber: 4, firstName: "Ibrahima", lastName: "Konaté", position: "Defender"),
+    footballPlayer(squadNumber: 5, firstName: "Andy", lastName: "Robertson", position: "Defender"),
+    footballPlayer(squadNumber: 6, firstName: "Alexis", lastName: "Mac Allister", position: "Midfielder"),
+    footballPlayer(squadNumber: 7, firstName: "Dominik", lastName: "Szoboszlai", position: "Midfielder"),
+    footballPlayer(squadNumber: 8, firstName: "Curtis", lastName: "Jones", position: "Midfielder"),
+    footballPlayer(squadNumber: 9, firstName: "Mohamed", lastName: "Salah", position: "Forward"),
+    footballPlayer(squadNumber: 10, firstName: "Darwin", lastName: "Núñez", position: "Forward"),
+    footballPlayer(squadNumber: 11, firstName: "Luis", lastName: "Díaz", position: "Forward"),
+  ];
 
-  void showTeam() {
-    print("The ${this.name} line up");
-    this.players.forEach((player){
-      print(${player.position} - ${player.getName()};
-    });
-  }
-}
+  // Creating football teams
+  footballTeam barcelona = footballTeam(name: "FC Barcelona", league: "La Liga", country: "Spain", players: barcelonaPlayers);
+  footballTeam liverpool = footballTeam(name: "Liverpool FC", league: "Premier League", country: "England", players: liverpoolPlayers);
 
-class footballPlayer {
-  int squadNumber;
-  String firstName;
-  String lastName;
-  String position;
-  footballPlayer({required this.squadNumber,required this.firstName,
-    required this.lastName,required this.position});
+  // Initializing a football game
+  footballGame match = footballGame(homeTeam: barcelona, awayTeam: liverpool);
 
-  String getName(){
-    return "${firstName.substring(0,1). ${lastName(0,1)}"
+  // Printing match details
+  print("Match: ${match.homeTeam.name} vs ${match.awayTeam.name}");
+  print("League: ${match.homeTeam.league} vs ${match.awayTeam.league}");
+  print("Country: ${match.homeTeam.country} vs ${match.awayTeam.country}");
+
+  liverpool.showTeam();
+  barcelona.showTeam();
+
+  for (int i = 0;i <= 90; i++) {
+    match.playGame(i);
+    sleep(Duration(milliseconds:500));
   }
 }
